fce88fd4a58e9479a05387dbf862d1a01f69312d
Taki <saihan0176@gmail.com>
2024-10-06 01:27:40 +0600

    add prompt config

--- dockershell.sh
+++ dockershell.sh
@@ -44,6 +44,17 @@ setopt PROMPT_SUBST
 PROMPT="%F{cyan}%n%f@%F{cyan}%m%f %F{blue}%2~%f %F{red}${vcs_info_msg_0_}%f$ "
 RPROMPT="%F{red}%t %w"
 
+#https://github.com/ohmyzsh/ohmyzsh/blob/master/themes/robbyrussell.zsh-theme
+
+# PROMPT="%(?:%{$fg_bold[green]%}%1{➜%} :%{$fg_bold[red]%}%1{➜%} ) %{$fg[cyan]%}%c%{$reset_color%}"
+# PROMPT+=" $(git_prompt_info)"
+
+# ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}(%{$fg[red]%}"
+# ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
+# ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}%1{$%}"
+# ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
+
+
 EOL
 
 
